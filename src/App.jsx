75b70ee1ffcf44fc9026272b14f287bf57a6dedb
Cesar J Frietas <teste@teste.com>
2023-12-05 18:08:18 -0300

    03_hooks - reatividade com multiplicacao entre dois valors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import UseState_02 from "./components/UseState_02";
+//import UseState_02 from "./components/UseState_02";
+import HookState_03 from "./components/UseState_03";
 
 export default function App() {
+
 
     return(
         <>
         <h1>REACT HOOKS</h1>
         {/*<UseState_01 />*/}
-        <UseState_02 />
+        {/*<UseState_02 />*/}
+        <HookState_03 />
         </>
     )
     
