c48f8df635f27480552987f2a5a96f79e69aaec7
Cesar J Frietas <teste@teste.com>
2023-12-05 15:53:26 -0300

    hook useState 01

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UseState_01 from "./components/UseState_01";
+
+export default function App() {
+
+    return(
+        <>
+        <h1>REACT HOOKS</h1>
+        <UseState_01 />
+        </>
+    )
+    
+}  
