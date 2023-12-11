cd49bdb658246156f49c2ac81e3a4f49271ba781
Cesar J Frietas <teste@teste.com>
2023-12-10 21:29:46 -0300

    Exercício compontentes 03

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import HookState_04 from './components/UseState_04'
+import HookState_05 from './components/UseState_05'
 
 export default function App() {
 
     return(
         <>
-        <h1>REACT HOOKS</h1>     
-        <HookState_04 />         
+        <h1>REACT HOOKS</h1>    
+    
+        <HookState_05 atualiza />         
       
         </>
     )
